66f38e8d48bb79f070ccaaf6ca0ecbc2f7e735d7
AndreJSON <andre.v.json@gmail.com>
2017-09-19 22:20:37 +0200

    Add basic line wrap.

--- formatter.py
+++ formatter.py
@@ -1,6 +1,19 @@
 import sys
+LINE_LIMIT = 70
 
 
-with open(sys.argv[1], "r") as txt: 
-	for line in txt:
-		print line,
+def processLine(fileLine):
+	while len(fileLine) > 0:
+		line = fileLine[:LINE_LIMIT]
+		cutOff = line.rfind(" ")
+		if cutOff == -1:
+			cutOff = LINE_LIMIT
+		print(fileLine[:cutOff], end="")
+		fileLine = fileLine[cutOff:]
+
+def main():
+	with open(sys.argv[1], "r") as txt: 
+		for fileLine in txt:
+			processLine(fileLine)
+
+main()
